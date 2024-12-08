b14e21e2a747885b043c4727e32c22fc68c61e89
opop <kak_y_vas_dela>
2024-12-08 22:15:56 +0700

    v4.2.0

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+# set -e
+
+bin_scripts="/usr/local/bin/"
+scripts_dir="./config_files/"
+
+for arg in "$@"; do
+	if [ "$arg" == "-c" ] || [ "$arg" == "--clean" ] || [ "$arg" == "--clear" ]; then
+		rm -rf $scripts_dir bin
+		break
+	fi
+done
+
+
+mkdir -p $scripts_dir 
+
+cp -r $bin_scripts . 
+cp -r $HOME/.config/alacritty/* $scripts_dir
+cp -r $HOME/.config/bspwm/ $scripts_dir
+cp -r $HOME/.config/polybar/ $scripts_dir
+cp -r $HOME/.config/sxhkd/* $scripts_dir
+cp -r $HOME/.zshrc $scripts_dir/zshrc
+
+# Options:
+cp -r $HOME/.config/nvim/ $scripts_dir
